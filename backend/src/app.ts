41511af1f20f72bfe632e8f882d6fc5a18c0dfdd
swapnil sahu <swapnilvp846@gmail.com>
2025-12-04 15:58:18 +0530

    users routes, solved dotenv import hoisting problem

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,19 @@
-import express from 'express';
-const app = express();
+import express, { Application, Request, Response } from 'express';
+import cors from "cors";
+
+const app: Application = express();
 const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+import usersRouter from "./routes/users-router.ts"
+app.use("/api/v1/users", usersRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
+    return console.log(`server is listening at http://localhost:${port}`);
 });
