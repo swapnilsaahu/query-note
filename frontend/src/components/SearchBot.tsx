7d4ee5364567f8b53e0978bcfa5f725b7bbdccde
swapnil sahu <swapnilvp846@gmail.com>
2025-12-28 21:47:30 +0530

    ui changes

--- frontend/src/components/SearchBot.tsx
+++ frontend/src/components/SearchBot.tsx
@@ -19,17 +19,18 @@ const SearchBot = () => {
             });
             if (!res.data) return;
             setChatHistory(prev => [...prev, searchQuery, res.data.responseText]);
+            setSearchQuery('');
         } catch (error) {
             console.error(error, "error while fetching llmresponse");
         }
     }
     return (
         <div>
-            <div>
-                {chatHistory.map((chat, index) => <div key={index}>{chat}</div>)}
+            <div className="mb-50">
+                {chatHistory.map((chat, index) => <pre key={index} className="text-wrap mx-40 my-10 border-black border-2 p-2 ">{chat}</pre>)}
             </div>
-            <div className="flex rounded-2xl bg-gray-400 text-black text-2xl h-16 fixed bottom-0 left-1/6 w-3/4 mb-8">
-                <input defaultValue="query chatbot" onChange={e => setSearchQuery(e.target.value)} value={searchQuery} className="w-full mx-2"></input>
+            <div className="flex rounded-2xl bg-gray-400 text-black text-2xl h-16 fixed bottom-0 left-1/5 w-3/4 mb-8 shadow-xl">
+                <input defaultValue="query chatbot" onChange={e => setSearchQuery(e.target.value)} value={searchQuery} className="w-full mx-2 focus:outline-none p-4 rounded-2xl"></input>
                 <IoMdSend className="text-4xl mt-3 mx-2" onClick={updateChat} />
             </div>
         </div>
